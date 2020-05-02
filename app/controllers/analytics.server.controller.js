fade4e711dac103bc4315d528d26c57cba9ace29
Jen Woo <jwoo4956@uni.sydney.edu.au>
2020-05-02 23:38:23 +1000

    Addtional query

--- app/controllers/analytics.server.controller.js
+++ app/controllers/analytics.server.controller.js
@@ -7,7 +7,7 @@ module.exports.getTopArticleRevisions = function(req, res) {
 
     Revision.findHighestRevisions(noOfArticles, function(error, result) {
         if (error) {
-            console.log("error")
+            console.log("Cannot find highest revision")
         } else {
             console.log(result)
             res.json(result);
@@ -20,7 +20,59 @@ module.exports.getLowestArticleRevisions = function(req, res) {
 
     Revision.findLowestRevisions(noOfArticles, function(error, result) {
         if (error) {
-            console.log("error")
+            console.log("Cannot find lowest revision")
+        } else {
+            console.log(result)
+            res.json(result);
+        }
+    })
+}
+
+module.exports.getLargestArticleGroup = function(req, res) {
+    noOfArticles = 2;
+
+    Revision.findEditLargestGroup(noOfArticles, function(error, result) {
+        if (error) {
+            console.log("Cannot find largest group")
+        } else {
+            console.log(result)
+            res.json(result);
+        }
+    })
+}
+
+module.exports.getSmallestArticleGroup = function(req, res) {
+    noOfArticles = 2;
+
+    Revision.findEditSmallestGroup(noOfArticles, function(error, result) {
+        if (error) {
+            console.log("Cannot find smallest group")
+        } else {
+            console.log(result)
+            res.json(result);
+        }
+    })
+}
+
+module.export.getLongestHistory = function(req, res) {
+    noOfArticles = 2;
+
+    Revision.findLongestHistory(noOfArticles, function(error, result) {
+        if (error) {
+            console.log("Cannot find longest history")
+        } else {
+            console.log(result)
+            res.json(result);
+        }
+    })
+}
+
+module.export.getShortestHistory = function(req, res) {
+    noOfArticles = 2;
+
+    Revision.findShortestHistory(noOfArticles, function(error, result) {
+        if (error) {
+            console.log("Cannot find shortest history")
         } else {
             console.log(result)
             res.json(result);
--- app/models/article.js
+++ app/models/article.js
@@ -10,21 +10,70 @@ var RevisionSchema = new mongoose.Schema(
 		 	versionKey: false
 		})
 
-// Query to find the top n articles with highest revisions
-RevisionSchema.statics.findHighestRevisions = function(noOfArticles, callback){
-    return this.find()
-    .select('title')
-	.sort({'timestamp':-1})
-	.limit(noOfArticles)
-	.exec(callback)
+// Query to find the top 2 articles with highest revisions
+RevisionSchema.statics.findHighestRevisions = function(noOfArticle, callback){
+
+	return this.aggregate([
+		{$group : {_id : {title : "$title"}, count : {$sum : 1}}},
+		{$sort 	: {count:-1}},
+		{$limit : noOfArticle}
+	]).exec(callback)
 }
 
-RevisionSchema.statics.findLowestRevisions = function(noOfArticles, callback){
+// Query to find the top 2 articles with lowest revisions
+RevisionSchema.statics.findLowestRevisions = function(noOfArticle, callback){
 	
-	return this.find()
-	.sort({'timestamp':-1})
-	.limit(noOfArticles)
-	.exec(callback)
+	return this.aggregate([
+		{$group : {_id : {title : "$title"}, count : {$sum : 1}}},
+		{$sort 	: {count:1}},
+		{$limit : noOfArticle}
+	]).exec(callback)
+}
+
+
+// Query to find the top 2 articles edited by the largest group of registered users
+RevisionSchema.static.findEditLargestGroup = function(noOfArticle, callback){
+
+	return this.aggregate([
+		{$group : {_id : {"title" : "$title", "user" : "$user"}, count : {$sum : 1}}},
+		{$group : {_id : "$_id.title", uniqueTitleCount : {$sum : 1}}},
+		{$sort 	: {uniqueTitleCount : -1}},
+		{$limit : noOfArticle}
+	]).exec(callback)
+}
+
+
+// Query to find the top 2 articles edited by the smallest group of registered users
+RevisionSchema.static.findEditSmallestGroup = function(noOfArticle, callback) {
+
+	return this.aggregate([
+		{$group : {_id : {"title" : "$title", "user" : "$user"}, count : {$sum : 1}}},
+		{$group : {_id : "$_id.title", uniqueTitleCount : {$sum : 1}}},
+		{$sort 	: {uniqueTitleCount : 1}},
+		{$limit : noOfArticle}
+	]).exec(callback)
+}
+
+// Query to find top 3 articles with the longest history
+RevisionSchema.static.findLongestHistory = function(noOfArticle, callback) {
+
+	return this.aggregate([
+		{$group : {_id : "$title", minTimestamp : {$min : "$timestamp"}}},
+		{$sort 	: {minTimestamp : 1}},
+		{$limit : noOfArticle}
+
+	]).exec(callback)
+}
+
+// Query to find top 3 articles with the shortest history
+RevisionSchema.static.findShortestHistory = function(noOfArticle, callback) {
+
+	return this.aggregate([
+		{$group : {_id : "$title", minTimestamp : {$min : "$timestamp"}}},
+		{$sort 	: {minTimestamp : -1}},
+		{$limit : noOfArticle}
+
+	]).exec(callback)
 }
 
 var Revision = mongoose.model('Revision', RevisionSchema, 'articles')
