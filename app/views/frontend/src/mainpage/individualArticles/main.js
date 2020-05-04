919ff074573736ba23037d1d15ef21041f282976
Rita <rche4499@uni.sydney.edu.au>
2020-05-04 14:59:16 +1000

    top 5 users connection, added pie chart example

--- app/views/frontend/src/mainpage/individualArticles/main.js
+++ app/views/frontend/src/mainpage/individualArticles/main.js
@@ -1,12 +1,22 @@
 import React, { useState, Component, useEffect } from "react";
-import {ArticleHeading, SubHeading, Result} from "./styled";
+import {ArticleHeading, SubHeading, Result, Chart, UserTable, ArticleSelect} from "./styled";
 import Select from '@atlaskit/select';
+import { Pie } from 'react-chartjs-2';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 
 export const IndividualArticles = () => {
   const [allArticles, setAllArticles] = useState([]);
   const [currentArticle, setCurrentArticle] = useState([]);
   const [currentArticleTitle, setCurrentArticleTitle] = useState([]);
+  const [topFiveUsers, setTopFiveUsers] = useState([]);
+  const [topThreeNews, setTopThreeNews] = useState([]);
+
    // Retrieve list from Express App
    useEffect(() => {
     fetch('/api/individual/getAllArticles').then(res => res.json()).then(list => setAllArticles(list));
@@ -20,26 +30,90 @@ export const IndividualArticles = () => {
   const articleSelected = (value) => {
       setCurrentArticleTitle(value._id.title);
       setCurrentArticle(value);
+      fetch('/api/individual/getTopFiveUsers/?title=' + currentArticleTitle).then(res => res.json()).then(list => setTopFiveUsers(list));
   }
+
+  var topFiveUsersTable = topFiveUsers.map(user => {
+    // return (<div><a>Username: {user._id.user} Count: {user.userCount}</a><br></br></div>)
+    return (
+      <TableBody>
+      <TableRow>
+        <TableCell align="right">1</TableCell>
+        <TableCell align="right">{user._id.user}</TableCell>
+        <TableCell align="right">{user.userCount}</TableCell>
+      </TableRow>
+  </TableBody>
+    )
+  })
 
     return (
         <div>
         <ArticleHeading>Individual Articles</ArticleHeading>
-
+        <ArticleSelect>
         <Select 
           onChange = {e => articleSelected(e.value)}
           options = {allArticlesOptions}
           placeholder = "Select an article...">
           </Select>
+          </ArticleSelect>
 
-         <SubHeading>Summary Information</SubHeading>
+          <SubHeading>Summary Information</SubHeading>
         <Result>
        
         <a><b>Title:</b> {currentArticleTitle}</a>
         <br></br>
         <a><b>Total Number of Revisions:</b> {currentArticle.count}</a>
         <br></br>
+        <a><b>Top 5 Regular Users:</b></a>
+
+        <UserTable>
+        <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="right"><b>Rank</b></TableCell>
+            <TableCell align="right"><b>User</b></TableCell>
+            <TableCell align="right"><b>Revision Count</b></TableCell>
+          </TableRow>
+        </TableHead>
+        {topFiveUsersTable}
+
+      </Table>
+      </UserTable>
+        
+        <br></br>
+
+        <a><b>Top 3 News Articles:</b></a>
+
         </Result>
+
+        <a><b>Bar Chart Showing Yearly Revision Number Distribution:</b></a>
+
+        <br></br>
+        
+        <div>
+        <a><b>Pie Chart Showing User Type Distribution:</b></a>
+        <Pie
+        data={{
+          labels: ['User Type 1', 'User Type 2', 'UserType 3'],
+          datasets: [{
+            data: [2000, 4000, 2450],
+            backgroundColor: ['red', 'blue', 'green']
+          }]
+        }}
+        height='75%'/>
+        
+        </div>
+
+        <br></br>
+    
+       
+        <a><b>Bar Chart:</b></a>
+
+        
+        
+       
+
+        
 
     </div>
 
--- app/views/frontend/src/mainpage/individualArticles/styled.js
+++ app/views/frontend/src/mainpage/individualArticles/styled.js
@@ -25,5 +25,38 @@ export const Result = styled.div`
     margin-bottom: 50px;
     margin-right: 150px;
     margin-left: 80px;
+`;
 
+export const ArticleSelect = styled.div`
+font-family: 'Roboto';
+font-size: 20px;
+background: white;
+color:black;
+padding: 10px;
+line-height: 300%;
+margin-top: 50px;
+margin-bottom: 50px;
+margin-right: 150px;
+margin-left: 80px;
+`;
+
+export const UserTable = styled.div`
+font-family: 'Roboto';
+font-size: 20px;
+background: white;
+width: 500px;
+`;
+
+export const Chart = styled.div`
+    font-family: 'Roboto';
+    font-size: 20px;
+    background: white;
+    color:white;
+    padding: 50px;
+    width: 300px;
+    height: 300px;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    margin-right: 150px;
+    margin-left: 80px;
 `;
