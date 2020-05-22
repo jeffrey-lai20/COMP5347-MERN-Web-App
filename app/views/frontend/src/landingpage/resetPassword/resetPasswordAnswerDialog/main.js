06775ad35357547a595892185e7dff189e04406f
Jeffrey Lai <lilxjeff@gmail.com>
2020-05-23 02:22:19 +1000

    Cleaning up code

--- app/views/frontend/src/landingpage/resetPassword/resetPasswordAnswerDialog/main.js
+++ app/views/frontend/src/landingpage/resetPassword/resetPasswordAnswerDialog/main.js
@@ -3,8 +3,29 @@ import Button, { ButtonAppearances } from '@atlaskit/button';
 import Textfield from '@atlaskit/textfield';
 import Tag from '@atlaskit/tag';
 import Modal, { ModalTransition } from '@atlaskit/modal-dialog';
+import {Result} from "../../../mainpage/authorAnalytics/styled";
 export const ResetPasswordAnswer = ({ResetPasswordAnswer}) => {
     const [isOpen, setIsOpen] = useState("");
+
+    // const [question, setQuestion] = useState([]);
+    // const [userSelected, setUser] = useState([]);
+    // const [allUsers, setAllUsers] = useState([]);
+
+    // useEffect(() => {
+    //     // GET request
+    //     fetch('/api/user/getQuestion').then(res => res.json()).then(list => setQuestion(list));
+    //     fetch('/api/user/setUser').then(res => res.json()).then(list => setUser(list));
+    //
+    // }, [])
+    //
+    // const userSelect = (value) => {
+    //     setUser(value._id.user);
+    //     fetch('/api/user/setUser/?user=' + value._id.user).then(res => res.json()).then(list => setUser(list));
+    // }
+    // const questionDisplay = userSelected.map(userData => {
+    //     return (<Result><b>Question:</b>{question}</Result>)
+    // })
+
     return (
         <div>
             <Button appearance="primary" className="button" type="submit" value="Continue" onClick={() => setIsOpen(true)} >Continue</Button>
@@ -16,6 +37,7 @@ export const ResetPasswordAnswer = ({ResetPasswordAnswer}) => {
                                 <Tag text="Username:" color="greyLight"/>
                                 <Textfield className="form-control" type="text" placeholder="Username" name="userName" required/>
                             </div>
+                            {/*{questionDisplay}*/}
                             <div>
                                 <Tag text="Answer:" color="greyLight"/>
                                 <Textfield className="form-control" type="text" placeholder="Answer" name="resetAnswer" required/>
