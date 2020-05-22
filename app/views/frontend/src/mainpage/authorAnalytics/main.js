f80d56a9c67ded183e9b57bfa0a096e172d7e8a2
Jeffrey Lai <lilxjeff@gmail.com>
2020-05-22 20:58:19 +1000

    Author autcomplete search fully functional

--- app/views/frontend/src/mainpage/authorAnalytics/main.js
+++ app/views/frontend/src/mainpage/authorAnalytics/main.js
@@ -1,10 +1,7 @@
 import React, { useState, Component, useEffect } from "react";
 import { ArticleHeading, SubHeading, Result} from "./styled"
-import Select from '@atlaskit/select';
-import Button, { ButtonAppearances } from '@atlaskit/button';
-import Textfield from '@atlaskit/textfield';
-import Tag from '@atlaskit/tag';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import {TextField} from "@material-ui/core";
 
 
 export const AuthorAnalytics = () => {
@@ -16,10 +13,7 @@ export const AuthorAnalytics = () => {
         // GET request
         fetch('/api/author/getAllAuthors').then(res => res.json()).then(list => setAllAuthors(list));
         fetch('/api/author/getAuthor').then(res => res.json()).then(list => setCurrentAuthor(list));
-
     }, [])
-
-
 
     const allAuthorOptions = allAuthors.map(article => ({
         label: "Author: " + article._id.user,
@@ -28,54 +22,34 @@ export const AuthorAnalytics = () => {
 
     const authorSelected = (value) => {
         setCurrentAuthorUser(value._id.user);
-        // console.log(value._id.user);
         fetch('/api/author/getAuthor/?user=' + value._id.user).then(res => res.json()).then(list => setCurrentAuthor(list));
-
-        // GET request
     }
 
     const authorCurrentDisplay = currentAuthor.map(article => {
         return (<Result><b>Author:</b>{article._id.user}<b><br/>Article:</b> {article._id.title} <br></br><b>Number of Revisions:</b> {article.count} </Result>)
     })
 
-
     return (
         <div>
             <ArticleHeading>Author Article Analytics</ArticleHeading>
             <div>
-                <Tag text="Author Search:" color="greyLight"/>
-                <Textfield className="form-control" type="text" placeholder="Author's Name" name="authorName" required/>
-                <Select
-                    onChange={e => authorSelected(e.value)}
+                <Autocomplete
+                    onChange={(event, valueSelected) => {
+                        authorSelected(valueSelected.value)
+                    }}
                     options={allAuthorOptions}
-                    placeholder="Select an author...">
-                </Select>
-
-                {/*<Autocomplete*/}
-                {/*    name="authorName"*/}
-                {/*    placeholder="Author's Name"*/}
-                {/*    options={allAuthorsOptions}*/}
-                {/*    getOptionLabel={(option)=>option.title}*/}
-                {/*    style={{ width: 300}}*/}
-                {/*    renderInput={(params) => <Textfield {...params} label="Combo box" variant="outlined" />}*/}
-                {/*    />*/}
+                    getOptionLabel={(option) => option.label}
+                    style={{ width: 500 }}
+                    renderInput={(params) => <TextField {...params} label="Search for an Author" variant="outlined" />}
+                />
             </div>
-            <Button appearance="primary" className="button" type="submit" value="Search">Search</Button>
-            <Button appearance="primary" className="button" type="reset" value="Clear">Clear</Button>
-
-            <SubHeading>Articles the author has written/edited</SubHeading>
-            {/*{allAuthorsOptions}*/}
-            {console.log(currentAuthorUser)}
             {currentAuthorUser != ""
                 ? <div>
-                    <h1>Author stuff: </h1>
+                    <SubHeading>Articles the author has written/edited</SubHeading>
                     {authorCurrentDisplay}
-
                 </div>: <div></div>
 
             }
-
-
         </div>
     );
 }
